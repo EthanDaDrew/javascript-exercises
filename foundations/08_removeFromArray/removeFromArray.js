cc790692de7a867377ceac413f131e88c1e659c4
ethandadrew <ethandadrew@gmail.com>
2026-01-18 00:17:32 -0600

    second round of exercises

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...valuesToRemove) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < valuesToRemove.length; j++) {
+            if (array[i] === valuesToRemove[j]) {
+                array.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
+    return array;
+  // array → [1, 2, 3, 4]
+  // valuesToRemove → [3, 1]
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    returnNum = 0;
+    if (num2 === num1) {
+        return num1;
+    }
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+    if (num2 > num1) {
+        for (let i = num1; i <= num2; i++) {
+            returnNum += i;
+        }
+    } else {
+        for (let i = num2; i <= num1; i++) {
+            returnNum += i;
+        }
+    }
+    return returnNum;
 };
 
 // Do not edit below this line
